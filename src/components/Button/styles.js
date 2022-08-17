021e2d394d8e50a65a38a92e046bd620313d21dd
deataide <deividdimasp4@hotmail.com>
2022-08-16 23:21:24 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+
+cursor: pointer;
+border: none;
+outline: none;
+border-radius: 5px;
+font-weight: 600;
+font-size: 16;
+max-width: 350px;
+padding: 16px 20px;
+color: white;
+background-color: #046ee5;
+width: 100%;
+  
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from 'styled-components';
+
+const Container = createGlobalStyle`
+  
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;}
+
+body{
+    background-color: #f0f2f5;
+    font-size: 12;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+
+`;
+
+export default Container
